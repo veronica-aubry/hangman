12983da2fe63f0de4e5185a5957c2a929307b695
Ronando Long <ronando.long@gmail.com>
2016-01-21 19:07:14 -0800

    Added comments and shit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
+// Business Logic
 // var helloWorld = function(){
 //   return false;
+//
+//   For loops and shit go here
+//   If/else like a madman here
+//
+//  return shit like a mo-fo here
+//
+//   ...whatchyu lookin at bitch? Green those specs and git back to work!
+//  Time fo yo Interface Logic, then suckface when it's all green.
 // };
+
+
+
+
+
+
+
+
+// User Interface Logic
+// $(document).ready(function() {
+//   $("form#IDselector").submit(function(event) {
+//     // variables and shit go here, bitch!
+//
+//     // link your muthafuckin' variables to yo bad-ass bidness logic function, Yo!
+//
+//     // Make shit happen here...BITCH!
+//
+//     // Muthafuckin' variables connect to your output down here hommie.
+//
+//     // Show me some money, hunny, and reveal that hiddin fine shit you got stashed.
+//
+//     // It's all good baby.'
+//     event.preventDefault();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,3 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
 // describe('helloWorld', function(){
 //   it("is going to be false", function(){
 //     expect(helloWorld()).to.equal(false);
