c8b48913fd0114875e0cbb7929092a201b27a63f
Ronando Long <ronando.long@gmail.com>
2016-01-21 18:49:53 -0800

    Refactored folders and files.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// Uncomment this to see the example spec test pass. Delete if you don't need it!
+// var helloWorld = function(){
+//   return false;
+// };
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
+// describe('helloWorld', function(){
+//   it("is going to be false", function(){
+//     expect(helloWorld()).to.equal(false);
+//   });
+// });
