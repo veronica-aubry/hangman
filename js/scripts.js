b627ccc497323a2b26c4b1795ac731ab58fa8abf
Marika Allely and Veronica Alley <veronica.aubry.alley@gmail.com>
2016-01-27 15:31:56 -0800

    clean up js; add minor features

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ var words = ["hello","goodbye", "marmalade"]
 var wordSplit = [];
 var puzzleArray = [];
 var score = 0;
-//generates random word from words array and splits the characters up in its own array called wordSplit//
+//generates random word from words array and splits the characters up in its own array called wordSplit; then pushed the corresponding amount of underscores to the puzzleArray//
 
 function getRandomWord() {
 var randomWordIndex = Math.floor(Math.random() * (3 - 0)) + 0;
@@ -21,23 +21,7 @@ var randomWordIndex = Math.floor(Math.random() * (3 - 0)) + 0;
 }
 
 
-// var test = function(wordSplit) {
-//   var word = getRandomWord();
-//   var length = word.length;
-//   puzzleArray = blankMaker(length);
-//   return puzzleArray;
-// }
 
-
-//creates an array of blanks, with the number of blanks based on the random word's length//
-
-
-// var blankMaker = function(num) {
-//   for (var i = 0; i < num; i++) {
-//     puzzleArray.push("_");
-// }
-//   return puzzleArray;
-// }
 
 //finds user input letter in the wordSplit and returns indexes of all matches. Then replaces matching indexes on puzzleArray with the guessed letter//
 
@@ -52,10 +36,6 @@ var letterQuest = function(wordSplit, searchLetter){
   }
   return puzzleArray;
   }
-
-
-
-
 
 // checks score for duration of game and breaks when the losing condition is met.//
 
@@ -80,19 +60,22 @@ var letterQuest = function(wordSplit, searchLetter){
 // }
 
 
-
 // User Interface Logic
 $(document).ready(function() {
   getRandomWord();
+  $("span#puzzle-array").text(puzzleArray);
   $("form#hangman").submit(function(event) {
-    debugger;
     var searchLetter = $("input#letter").val();
     var searchLetter = searchLetter.toLowerCase();
-    Game.guessedLetters.push(searchLetter);
+    Game.guessedLetters.push(" " + searchLetter);
     var results = letterQuest(wordSplit, searchLetter);
+    $("span#puzzle-array").text(puzzleArray);
+    $("span#guessed-letters").text(Game.guessedLetters);
     console.log(results);
     console.log(wordSplit);
     console.log(Game.guessedLetters);
     event.preventDefault();
+
+    $("input#letter").val("");
   });
 });
