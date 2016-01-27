f6f2a4cbe24fecbb2c05ef3832bf5e62f45ca7b6
Marika Allely and Veronica Alley <mballely@gmail.com>
2016-01-27 15:27:50 -0800

    add scratchpaper to keep old code

---
+++ scratchpaper.js
@@ -0,0 +1,17 @@
+// var test = function(wordSplit) {
+//   var word = getRandomWord();
+//   var length = word.length;
+//   puzzleArray = blankMaker(length);
+//   return puzzleArray;
+// }
+
+
+//creates an array of blanks, with the number of blanks based on the random word's length//
+
+
+// var blankMaker = function(num) {
+//   for (var i = 0; i < num; i++) {
+//     puzzleArray.push("_");
+// }
+//   return puzzleArray;
+// }
